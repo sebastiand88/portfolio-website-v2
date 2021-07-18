3e47568f3124cefac65f720212095e6f1123bf9b
sebastiand88 <68199815+sebastiand88@users.noreply.github.com>
2021-07-18 20:44:07 +0100

    added react-movies-search project

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -3,7 +3,7 @@ export const projects = [
     title: "GitHub User Finder",
     description:
       "With the help of HTML, MaterializeCSS, JavaScript and GitHub API you can search for any user and their public repos.",
-    image: "/images/2.jpg",
+    image: "/images/github.jpg",
     tags: ["HTML", "Materialize", "JavaScript"],
     source: "https://sebastiand88.github.io/githubFinder/",
     visit: "https://github.com/sebastiand88/githubFinder",
@@ -13,7 +13,7 @@ export const projects = [
     title: "Quiz Time!",
     description:
       "This React Trivia application was one of the funest projects I've done so far. Just enter your name and have fun.",
-    image: "/images/5.jpg",
+    image: "/images/quiz.jpg",
     tags: ["React", "CSS"],
     source: "https://react-quiz-time-app.netlify.app/",
     visit: "https://github.com/sebastiand88/react-quiz",
@@ -22,8 +22,8 @@ export const projects = [
   {
     title: "ECHO",
     description:
-      "A responsive website made to raise awareness and contribute in the fight against global warming, ocean pollution and CO2 gas emissions.",
-    image: "/images/3.jpg",
+      "Using React Router and Styled Components I've made a fully responsive website for an eco-friendly energy provider showcasing their ambitions and other services.",
+    image: "/images/echo.jpg",
     tags: ["React", "CSS"],
     source: "https://react-echo.netlify.app/",
     visit: "https://github.com/sebastiand88/ECHO",
@@ -33,11 +33,30 @@ export const projects = [
     title: "Saving Earth",
     description:
       "A responsive website made to raise awareness and contribute in the fight against global warming, ocean pollution and CO2 gas emissions.",
-    image: "/images/4.jpg",
+    image: "/images/earth.jpg",
     tags: ["React", "CSS"],
     source: "https://saving-earth.netlify.app/",
     visit: "https://github.com/sebastiand88/Saving_Earth",
     id: 3,
+  },
+  {
+    title: "Movie Search",
+    description:
+      "An easy way to search for movies using React and ThemoviesDB API. Search for a movie you want to watch read the description and see the overall movie rating.",
+    image: "/images/search.jpg",
+    tags: ["React", "CSS"],
+    source: "https://react-app-moviesearch.netlify.app/",
+    visit: "https://github.com/sebastiand88/movie-search",
+    id: 4,
+  },
+  {
+    title: "Notflix",
+    description: "",
+    image: "/images/notflix.jpg",
+    tags: ["React", "SCSS"],
+    source: "https://www.google.com/",
+    visit: "https://www.google.com/",
+    id: 5,
   },
 ];
 
